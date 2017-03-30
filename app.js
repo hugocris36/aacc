30d510ed90ff2b643eb8343a7f12d24c15d99953
Hugo Cristiano Lima dos Santos <hugocris@br.ibm.com>
2017-03-30 14:42:58 -0500

    configuracao

--- app.js
+++ app.js
@@ -21,6 +21,36 @@ app.use(express.static(__dirname + '/public'));
 // get the app environment from Cloud Foundry
 var appEnv = cfenv.getAppEnv();
 
+var watson = require('watson-developer-cloud');
+var tbot = require('node-telegram-bot-api');
+
+var conversation = watson.conversation({
+  username: 'e791fcfd-991d-4880-90b7-9a8f1bf80ee8',
+  password: 'xByoKxnw8Pif',
+  version: 'v1',
+  version_date: '2016-09-20'
+});
+
+var context = {};
+var telegramBot = new tbot('#TOKENTELEGRAM', { polling: true });
+
+telegramBot.on('message', function (msg) {
+	var chatId = msg.chat.id;	
+	
+conversation.message({
+		workspace_id: '42613812-2b42-4817-8900-9aea511',
+		input: {'text': msg.text},
+		context: context
+	},  function(err, response) {
+		if (err)
+			console.log('error:', err);
+		else{
+			context = response.context;
+			telegramBot.sendMessage(chatId, response.output.text[0]);
+		}
+	});	
+});
+
 // start server on the specified port and binding host
 app.listen(appEnv.port, '0.0.0.0', function() {
   // print a message when the server starts listening
